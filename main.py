3d8d5b65040cf8b7738780e3d2e442fc5166c825
Sebastian <sebaslozanosalas@gmail.com>
2024-07-19 13:53:10 -0600

    Some updates

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-from board import Board
-from player import Player
-
 from random import choice
 import os
+
+
+class Player:
+    def __init__(self, name: str, mark: str):
+        self.name = name
+        self.mark = mark
+
+
+class Board:
+    def __init__(self):
+        self.grid = [' ' for _ in range(9)]
+
+    def show(self):
+        print()
+        for i in range(0, 9, 3):
+            print(f' {self.grid[i]} | {self.grid[i+1]} | {self.grid[i+2]}      {i},{i+1},{i+2}')
+            if i < 6:
+                print(f'-----------')
+
+
+    def update_cell(self, player, loc):
+
+        if self.grid[loc] != ' ':
+            print('Opcion no valida')
+        else:
+            self.grid[loc] = player.mark
+
+
+
+
+class Game:
+    def __init__(self):
+        pass
 
 
 def print_header(*args):
@@ -52,3 +82,4 @@ board.show()
 # pick starter
 current_player = choice([p1, p2])
 
+get_player_choice(current_player, board)
