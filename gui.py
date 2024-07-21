4f0153526330d4471ce17c6ed12f5f5a29448127
Sebastian <sebaslozanosalas@gmail.com>
2024-07-20 20:11:53 -0600

    basic grid

--- gui.py
+++ gui.py
@@ -7,8 +7,9 @@ class GUI:
         self.width = 500
         self.height = 700
 
-        self.bg = '#262626'
-        self.bg_ligth = '#2C2C2C'
+        self.color_darker = '#262626'
+        self.color_dark = '#2C2C2C'
+        self.color_accent = '#FF914D'
 
         self.screen = None
         self.clock = None
@@ -19,6 +20,7 @@ class GUI:
 
     def setup(self):
         pygame.init()
+        pygame.display.set_caption('Tic Tac Toe by Seb')
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
 
@@ -34,8 +36,10 @@ class GUI:
         self.setup()
         while self.running:
             self.handle_events()
-            self.screen.fill(self.bg)
-            self.draw_rect()
+            self.screen.fill(self.color_darker)
+            # self.draw_rect()
+            self.draw_grid()
+            self.draw_title()
             pygame.display.flip()
             self.dt = self.clock.tick(self.fps) / 1000
         pygame.quit()
@@ -53,4 +57,45 @@ class GUI:
 
         my_rect = pygame.Rect(left_pos, top_pos, box_size, box_size)
 
-        pygame.draw.rect(self.screen, self.bg_ligth, my_rect)
+        pygame.draw.rect(self.screen, self.color_dark, my_rect)
+
+    def draw_grid(self):
+
+        grid_color = '#2F2F2F'
+
+        # game area square
+        screen_width, screen_height = self.screen.get_width(), self.screen.get_height()
+        margin = 0.02
+
+        box_size = screen_width - ((screen_width * margin) * 2)
+        grid_length = box_size
+        grid_width = screen_width * margin
+
+        # draw horizontal lines
+        left_pos = margin * screen_width
+        top_pos = screen_height - grid_width - ((box_size / 3) * 2) - (grid_width / 2)
+        my_rect = pygame.Rect(left_pos, top_pos, grid_length, grid_width)
+        pygame.draw.rect(self.screen, grid_color, my_rect)
+        
+        top_pos = screen_height - grid_width - ((box_size / 3) * 1)  - (grid_width / 2) # update
+        my_rect = pygame.Rect(left_pos, top_pos, grid_length, grid_width)
+        pygame.draw.rect(self.screen, grid_color, my_rect)
+
+
+        # draw vertical lines
+        left_pos = grid_width + ((box_size / 3) * 1) - (grid_width / 2)
+        top_pos = screen_height - (box_size + (screen_width * margin))
+        my_rect = pygame.Rect(left_pos, top_pos, grid_width, grid_length)
+        pygame.draw.rect(self.screen, grid_color, my_rect)
+        
+        left_pos = grid_width + ((box_size / 3) * 2) - (grid_width / 2)
+        my_rect = pygame.Rect(left_pos, top_pos, grid_width, grid_length)
+        pygame.draw.rect(self.screen, grid_color, my_rect)
+
+
+    def draw_title(self):
+        font = pygame.font.Font('freesansbold.ttf', 32)
+        title = font.render('Tic Tac Toe', True, self.color_accent)
+        titleRect = title.get_rect()
+        titleRect.center = (self.width // 2, 50)
+        self.screen.blit(title, titleRect)
